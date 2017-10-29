ee4684607d48c8ecfbafb480f1b28e2d817f26d0
Mikhail Grinshteyn <mgrinsht@gmail.com>
2017-10-29 18:02:38 -0400

    add proxy server

--- app/core/flaskServer.py
+++ app/core/flaskServer.py
@@ -1,64 +1,28 @@
-from flask import Flask
-from flask import Request
-from flask import Response
-
+from flask import Flask, Response
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/app1/*/app1/*')
-def appl1():
-    resp = Response('Hello World!')
-    #    resp = make_response()
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
-
-@app.route('/app2/*/app2/*')
-def appl2():
-    resp = Response('{"status": "success","message": "",  "payload": ['
-                    '{ "name": "Employees", "columns": ['
-                    '{"name": "EmployeeID","type": "int","size": 11,"primaryKey": "true","required": "true"}]]}')
-    #    resp = make_response()
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
-
-@app.route('/app3/*')
-def appl3():
-    resp = Response('{"status": "success","message": "",  "payload": ['
-                    '{ "name": "Employees", "columns": ['
-                    '{"name": "EmployeeID","type": "int","size": 11,"primaryKey": "true","required": "true"}]]}')
-    #    resp = make_response()
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
-
-@app.route('/app3/schemas')
+@app.route('/dmc/v1.0/schemas')
 def schemas():
     resp = Response('{"status": "success","message": "",  "payload": []}')
-    #    resp = make_response()
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
 @app.route('/dmc/v1.0/schemas/schema_id')
 def schema_id():
-#    req = Request
-#    req.headers['Access-Control-Allow-Origin'] = '*'
     resp = Response('{"status": "success","message": "",  "payload": ['
                     '{ "name": "Employees", "columns": ['
                     '{"name": "EmployeeID","type": "int","size": 11,"primaryKey": "true","required": "true"}]}]}')
-#    resp = make_response()
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
-#    return '{"status": "success","message": "",  "payload": [' \
-#           '{ "name": "Employees", "columns": [' \
-#           '{"name": "EmployeeID","type": "int","size": 11,"primaryKey": "true","required": "true"}]' \
-#           ']}'
-
-# dmc/v1.0/schemas/denorm
+@app.route('/dmc/v1.0/schemas/denorm')
+def denorm():
+    resp = Response('{"status": "success","message": "",  "payload": ['
+                    '{ "name": "Employees", "columns": ['
+                    '{"name": "EmployeeID","type": "int","size": 11,"primaryKey": "true","required": "true"}]}]}')
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 if __name__ == '__main__':
     app.run()
